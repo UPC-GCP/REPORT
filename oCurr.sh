626e2cd768a253944055b0ea802e9ae971252e44
UPC-GCP <gonzalo.cespedes@estudiantat.upc.edu>
2026-06-29 07:37:53 -0500

    ChapConvDiffDone

--- oCurr.sh
+++ oCurr.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-vim -p TFM_Report.tex ./Chapter2/TFM_Convection.tex ./Chapter2/TFM_Convection2D.tex ./Chapter1/TFM_Diffusion2D.tex
+vim -p TFM_Report.tex ./Chapter2/TFM_Convection.tex ./Chapter2/TFM_Convection2D.tex
